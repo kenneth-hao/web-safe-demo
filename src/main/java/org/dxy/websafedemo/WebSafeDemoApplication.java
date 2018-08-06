372e42a5130a462966e57f3e3f4ff5927e8d396c
郝跃文 <haoyw@dxyer.com>
2018-08-06 15:37:24 +0800

    git init

---
+++ src/main/java/org/dxy/websafedemo/WebSafeDemoApplication.java
@@ -0,0 +1,12 @@
+package org.dxy.websafedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSafeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSafeDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/org/dxy/websafedemo/WebSafeDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.dxy.websafedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebSafeDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
